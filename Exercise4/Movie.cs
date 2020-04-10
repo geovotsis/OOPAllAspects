e1503e8d958cd70fc5fac81632c97d64812dff1d
George <geovotsis@yahoo.gr>
2020-04-10 19:53:11 +0300

    First commit

---
+++ Exercise4/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Movie
+    {
+        public string title;
+        public DateTime release;
+        public string director;
+        public List<string> cast = new List<string>();
+
+        public bool SearchCast(string name)
+        {
+            for (int i = 0; i < cast.Count; i++)
+            {
+                if (cast[i] == name)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+
+}
+
+    
+
